551b35eb37725c12424fe70b209b83adb89cfc54
Viktor Rádi <rviktor87@web-fejlesztes.eu>
2021-11-24 11:06:07 +0100

    Creating a Todo class

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 console.log(
-`Parancssori Todo applikáció
-=============================
-
-Parancssori argumentumok:
-    -l   Kilistázza a feladatokat
-    -a   Új feladatot ad hozzá
-    -r   Eltávolít egy feladatot
-    -c   Teljesít egy feladatot`
+'Parancssori Todo applikáció\n\
+=============================\n\
+\n\
+Parancssori argumentumok:\n\
+    -l   Kilistázza a feladatokat\n\
+    -a   Új feladatot ad hozzá\n\
+    -r   Eltávolít egy feladatot\n\
+    -c   Teljesít egy feladatot'
 );
