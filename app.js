fdb1689f398b9407fbb650a43afaa1019342db26
Viktor Rádi <rviktor87@web-fejlesztes.eu>
2021-11-24 10:49:53 +0100

    User guide is written to the console

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log(
+`Parancssori Todo applikáció
+=============================
+
+Parancssori argumentumok:
+    -l   Kilistázza a feladatokat
+    -a   Új feladatot ad hozzá
+    -r   Eltávolít egy feladatot
+    -c   Teljesít egy feladatot`
+);
