df86a186595c929b890d01121ff6b02be2cf73b7
Viktor Rádi <rviktor87@web-fejlesztes.eu>
2021-11-25 11:56:07 +0100

    checking parameters

--- TodoApp.js
+++ TodoApp.js
@@ -1,9 +1,16 @@
 export default class TodoApp {
     #args;
+    #commands;
 
     constructor(args) {
         this.#args = args;
+        this.#commands = [
+            ["-l", this.list],
+            ["-a", this.addTodo, this.checkIfNextParameterIsACommand],
+            ["-r", this.remove, this.checkIfNextParameterIsACommand]
+        ];
     }
+
     list() {
         console.log("listing");
         // File beolvasást
@@ -11,15 +18,66 @@ export default class TodoApp {
         // rawTodoList => todoList = [ Todo{}, Todo{}, ]
         // a todoList kilistázása
     }
+
     print(string) {
         console.log(string);
     }
+
     run() {
+        /*
+        switch (this.#args[0]){
+            case "-l": {
+                this.list();
+                break;
+            }
+            case "-a": {
+                this.addTodo(this.#args[1]);
+            }
+        }
+         */
+
+        /*
         if (this.#args.includes('-l')) {
             this.list();
+        } else if (this.#args.includes('-a')) {
+            this.addTodo();
         } else {
+            this.printIntro();
+        }
+         */
 
-            const intro = 'Parancssori Todo applikáció\n\
+        this.#commands.forEach(([possibleParameter, functionToRun, validateFn]) => {
+            // const [possibleParameter, functionToRun] = commandPair;
+            // const possibleParameter = commandPair[0];
+            // const functionToRun = commandPair[1];
+            if (this.#args.includes(possibleParameter)) {
+                const indexOfPossibleParameter = this.#args.indexOf(possibleParameter)
+                const nextParameterValue = this.#args[indexOfPossibleParameter + 1];
+                if (validateFn === undefined) {
+                    functionToRun(this.#args[indexOfPossibleParameter + 1]);
+                } else if (validateFn(nextParameterValue, possibleParameter)) {
+                    functionToRun(this.#args[indexOfPossibleParameter + 1]);
+                }
+            }
+        });
+    }
+
+    checkIfNextParameterIsACommand(nextParameterValue, actualParameter) {
+        if (nextParameterValue === undefined ||
+            (nextParameterValue.length === 2 && nextParameterValue[0] === "-")) {
+            console.error(`Hiányzik egy paraméter a ${actualParameter} paraméter után`);
+            return false;
+        }
+        return true;
+    }
+
+    addTodo(todo) {
+        console.log(todo);
+        // console.log("adding Todo: " + todo);
+    }
+
+    printIntro() {
+        const intro = 'Parancssori Todo applikáció\n\
             =============================\n\
             \n\
             Parancssori argumentumok:\n\
@@ -28,7 +86,10 @@ export default class TodoApp {
             -r   Eltávolít egy feladatot\n\
             -c   Teljesít egy feladatot';
 
-            this.print(intro);
-        }
+        this.print(intro);
+    }
+
+    remove(id) {
+        console.log("removing a todo: " + id);
     }
 }
--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 import TodoApp from './TodoApp.js';
 const args = process.argv.slice(2);
 
+// Router - paraméter => if else helyettesíteni
+// Fájlbeolvasás
+// SRP = Single Responsibility Principle
+
 const app = new TodoApp(args);
 app.run();
